2472ad55ce1d37b27183c29da0ee9a87731643bc
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-01-14 23:50:37 +0530

    Changed .gitignore

--- server/dist/middlewares/middleware.js
+++
@@ -1,5 +0,0 @@
-import jwt from "jsonwebtoken";
-const secret = "mySecret";
-export function createJWT({ username, password, }) {
-    return jwt.sign({ username, password }, secret, { expiresIn: "1h" });
-}
